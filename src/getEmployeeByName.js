f007cd21c7fdd333df02f7fa36e933bab1b63563
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-07-28 15:49:43 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (typeof employeeName === 'undefined') {
+    return {};
+  }
+  return data.employees.find((element) =>
+    [element.firstName, element.lastName].includes(employeeName));
 }
 
 module.exports = getEmployeeByName;
