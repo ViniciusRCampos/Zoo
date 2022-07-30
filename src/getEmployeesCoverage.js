032996a3069021392926a03e3c40b14f6324d4b0
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-07-30 12:05:41 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const getSpecies = (id) =>
+  data.species.find((element) => element.id === id);
+
+const allEmployees = data.employees.map((element) => (
+  {
+    id: element.id,
+    fullName: `${element.firstName} ${element.lastName}`,
+    species: element.responsibleFor.map((animalId) => getSpecies(animalId).name),
+    locations: element.responsibleFor.map((animalId) => getSpecies(animalId).location),
+  }));
+
+function getEmployeesCoverage(object) {
+  if (!object) return allEmployees;
+
+  const find = allEmployees.find((element) =>
+    element.fullName.includes(Object.values(object)[0])
+    || element.id.includes(Object.values(object)[0]));
+
+  if (!find) {
+    throw new Error('Informações inválidas');
+  }
+  return find;
 }
+console.log(getEmployeesCoverage({ name: 'Nelson' }));
 
 module.exports = getEmployeesCoverage;
